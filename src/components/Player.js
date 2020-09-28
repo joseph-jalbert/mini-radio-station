5396336880d4db62b7001384ef4568fe1de44b9c
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2020-09-28 17:52:51 -0400

    kill stream when loading a new station

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 const ERROR_TEXT = 'Sorry, there was a problem trying to play this station. Please try another.';
 
-const Player = ({station, stationError}) => (
-  <div className="player" key={station.id}>
-    <h3>{station.name}</h3>
-    <div>{station.description}</div>
-    <br/>
-    <div><b>Categories:</b> {station.tags.join(', ')}</div>
-    <br/>
-    <div><img alt={station.name} src={station.imgUrl} /></div>
-    <audio
-      autoPlay={true}
-      controls
-      onError={() => {
-        alert(ERROR_TEXT);
-        stationError();
-      }}
-      src={station.streamUrl}>
-    </audio>
-  </div>
-);
+const Player = ({station, stationError}) => {
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    const htmlPlayer = playerRef.current;
+    return () => {
+      // stop audio stream on cleanup
+      htmlPlayer.src='';
+    };
+  }, [station]);
+
+  return (
+    <div  className="player" key={station.id}>
+      <h3>{station.name}</h3>
+      <div>{station.description}</div>
+      <br/>
+      <div><b>Categories:</b> {station.tags.join(', ')}</div>
+      <br/>
+      <div><img alt={station.name} src={station.imgUrl} /></div>
+      <audio
+        ref={playerRef}
+        autoPlay={true}
+        controls
+        onError={() => {
+          alert(ERROR_TEXT);
+          stationError();
+        }}
+        src={station.streamUrl}>
+      </audio>
+    </div>
+  );
+}
 
 
 export default Player;
